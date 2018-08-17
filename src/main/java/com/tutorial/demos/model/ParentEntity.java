4e9a4b49a1661b29a6bd2d423633103728b8a7d7
Fisiandroide <julio.limachi3@gmail.com>
2018-08-17 11:42:48 -0500

    Entity user Created

---
+++ src/main/java/com/tutorial/demos/model/ParentEntity.java
@@ -0,0 +1,36 @@
+package com.tutorial.demos.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+
+@MappedSuperclass
+@Access(AccessType.FIELD)
+public class ParentEntity implements Serializable {
+
+
+	
+	private static final long serialVersionUID = -3903335740994358949L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id", unique=true,nullable=false)
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	
+}
---
+++ src/main/java/com/tutorial/demos/model/User.java
@@ -0,0 +1,74 @@
+package com.tutorial.demos.model;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+@Access(AccessType.FIELD)
+public class User  extends ParentEntity{
+
+	
+	private static final long serialVersionUID = 159613586650476168L;
+
+   @Column(name="First_name",nullable=false,length=255)
+	private String  Firstname;
+   
+   @Column(name="second_name",nullable=true,length=255)
+	private String  secondname;
+   
+   @Column(name="first_surname",nullable=false,length=255)
+	private String  firstsurname;
+   
+   @Column(name="second_surname",nullable=true,length=255)
+	private String  secondsurname;
+   
+   @Column(name="phone",nullable=false,length=150)
+	private String  phone;
+   
+   @Column(name="address",nullable=false,length=150)
+	private String  address;
+	
+	
+	public String getFirstname() {
+		return Firstname;
+	}
+	public String getSecondname() {
+		return secondname;
+	}
+	public String getFirstsurname() {
+		return firstsurname;
+	}
+	public String getSecondsurname() {
+		return secondsurname;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setFirstname(String firstname) {
+		Firstname = firstname;
+	}
+	public void setSecondname(String secondname) {
+		this.secondname = secondname;
+	}
+	public void setFirstsurname(String firstsurname) {
+		this.firstsurname = firstsurname;
+	}
+	public void setSecondsurname(String secondsurname) {
+		this.secondsurname = secondsurname;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+}
